4a81d66bbfbe905e290108df9cce527341e29cc1
Anjaneya Reddy (IGIDR, Mumbai) <areddy.ragini@gmail.com>
2025-11-23 10:01:15 +0530

    Update llm_chain.py

--- llm_chain.py
+++ llm_chain.py
@@ -1,6 +1,7 @@
 import streamlit as st
+
 from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 def setup_qa_chain(vectorstore):
